69f4309197ce9df53bb026b87d8bc9a24fcfd75d
Benjamin Elfen <benjamin.elfen@boehringer-ingelheim.com>
2023-02-15 09:15:15 +0100

    added bi eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,9 +4,14 @@ module.exports = {
         "es2021": true
     },
     "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+        // Standard rules should be in BI package
+        // "eslint:recommended",
+        // "plugin:react/recommended",
+        // "plugin:@typescript-eslint/recommended",
+        "@boehringer-ingelheim/eslint-config/base/strict",
+        "@boehringer-ingelheim/eslint-config/react",
+        // Enable when using playwright
+        // "@boehringer-ingelheim/eslint-config/playwright"
     ],
     "overrides": [
     ],
