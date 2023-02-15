e67ea99943a96b9df4437119e1fc7799b9cc0936
Benjamin Elfen <benjamin.elfen@boehringer-ingelheim.com>
2023-02-15 09:30:35 +0100

    yarn prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,24 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        // Standard rules should be in BI package
-        // "eslint:recommended",
-        // "plugin:react/recommended",
-        // "plugin:@typescript-eslint/recommended",
-        "@boehringer-ingelheim/eslint-config/base/strict",
-        "@boehringer-ingelheim/eslint-config/react",
-        // Enable when using playwright
-        // "@boehringer-ingelheim/eslint-config/playwright"
-    ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    // Standard rules should be in BI package
+    // "eslint:recommended",
+    // "plugin:react/recommended",
+    // "plugin:@typescript-eslint/recommended",
+    '@boehringer-ingelheim/eslint-config/base/strict',
+    '@boehringer-ingelheim/eslint-config/react',
+    // Enable when using playwright
+    // "@boehringer-ingelheim/eslint-config/playwright"
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {},
+};
