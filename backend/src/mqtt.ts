ccba98432d31447479a6c56c83c0c50f8b881113
Benjamin Elfen <benjamin.elfen@boehringer-ingelheim.com>
2023-02-17 23:23:52 +0100

    started with mqtt

---
+++ backend/src/mqtt.ts
@@ -0,0 +1,64 @@
+import mqtt from 'mqtt';
+import { storeTasmotaDevice } from './storage';
+import { TasmotaDevice } from './tasmota.types';
+
+export function mqttSetup() {
+  const mqttUrl = process.env.MQTTURL ?? 'localhost';
+  const mqttPort = process.env.MQTTPORT ?? '1883';
+  const mqttFullPath = 'mqtt://' + mqttUrl + ':' + mqttPort;
+
+  const client = mqtt.connect(mqttFullPath);
+
+  // Topics to subscribe to
+  const tasmotaTopic = process.env.TASMOTATOPIC ?? 'tasmota';
+  const fullTasmotaTopic = tasmotaTopic + '/discovery/+/config';
+
+  client.on('connect', () => {
+    console.log('connected to MQTT broker with path: ' + mqttFullPath + '\n');
+
+    client.subscribe(fullTasmotaTopic);
+    console.log('subscribed to topic: ' + fullTasmotaTopic + '\n');
+
+    client.publish('network-overview2', 'api online');
+  });
+
+  client.on('message', (topic, message) => {
+    // console.log('message: ' + message, ' on topic: ' + topic);
+    if (topic.includes(tasmotaTopic)) {
+      handleTasmotaMessage(message.toString());
+    }
+  });
+
+  client.on('error', (err) => {
+    console.warn('An error within the mqtt connection: ' + err.message);
+  });
+
+  client.on('disconnect', () => {
+    client.publish('network-overview2', 'api offline - disconnect');
+    // client.end();
+  });
+}
+
+function handleTasmotaMessage(message: string) {
+  // console.log('message: ' + message + '\n');
+  const data = JSON.parse(message);
+
+  // check if valid ip
+  if (data.ip) {
+    const device: TasmotaDevice = {
+      ip: data.ip,
+      deviceName: data.dn,
+      friendlyName: data.hn,
+      mac: data.mac,
+      deviceType: data.md,
+      swVersion: data.sw,
+      topic: data.t,
+      fullTopic: data.ft,
+      subTopics: data.tp,
+      // stateVariants: data.state,
+    };
+
+    // add device to storage when not already there
+    storeTasmotaDevice(device);
+  }
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,22 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
-import express, { Express, Request, Response } from 'express';
+import express, { Express, NextFunction, Request, Response } from 'express';
+
+import { mqttSetup } from './mqtt';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+mqttSetup();
+
 app.use(cors());
+
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 app.get('/', (_req: Request, res: Response) => {
   res.send('API for network-overview2');
