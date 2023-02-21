9551eaf8d65d6f668909da7fcdf0dd2394e1574d
Benjamin Elfen <benjamin.elfen@boehringer-ingelheim.com>
2023-02-21 15:31:23 +0100

    get all route

--- backend/src/routes/tasmota.ts
+++ backend/src/routes/tasmota.ts
@@ -1,25 +1,22 @@
 import express, { NextFunction, Request, Response } from 'express';
-import { addDevicesToStore, discoveredDevices, saveToStorage } from '../storage';
+import { addDevicesToStore, discoveredDevices } from '../storage';
+import { tasmotaDevices } from './../storage';
 import { TasmotaDevice } from './../tasmota.types';
 
 export const tasmotaRouter = express.Router();
+
+tasmotaRouter.get('/', (_req: Request, res: Response, _next: NextFunction) => {
+  res.status(200).send(tasmotaDevices);
+});
 
 tasmotaRouter.get('/discover', (_req: Request, res: Response, _next: NextFunction) => {
   res.status(200).send(discoveredDevices);
 });
 
-tasmotaRouter.post('/save-discovered', (_req: Request, res: Response, _next: NextFunction) => {
-  const err = saveToStorage(discoveredDevices);
-  if (err) {
-    res.status(500).send(err);
-  }
-  res.status(200).send();
-});
-
 tasmotaRouter.post('/add', (req: Request, res: Response, _next: NextFunction) => {
   const newDevices: TasmotaDevice[] = req.body;
 
-  addDevicesToStore(newDevices)
+  addDevicesToStore(newDevices);
 
   res.status(200).send();
 });
